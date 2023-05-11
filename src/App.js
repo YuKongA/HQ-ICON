5d61c5d99a4238fd1b8b4589d34a3999264695d5
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-05-11 20:20:24 +0800

    fix: 调整优化
    例：https://icon.yukonga.top?name=星穹铁道&country=cn&device=ios&cut=1&limit=1

--- src/App.js
+++ src/App.js
@@ -12,22 +12,24 @@ import './App.css';
 class App extends Component {
     constructor(props) {
         super(props);
-        if (getUrlArgs('c') != "") { var c = getUrlArgs('c') } else var c = 'CN';
-        if (getUrlArgs('l') != "") { var l = getUrlArgs('l') } else var l = 10;
-        if (getUrlArgs('r') != "") { var r = getUrlArgs('r') } else var r = '1';
+        if (getUrlArgs('country') != "") { var c = getUrlArgs('country') } else var c = 'cn';
+        if (getUrlArgs('device') != "") { var d = getUrlArgs('device') } else var d = 'ios';
+        if (getUrlArgs('cut') != "") { var r = getUrlArgs('cut') } else var r = '1';
+        if (getUrlArgs('limit') != "") { var l = getUrlArgs('limit') } else var l = 10;
         this.state = {
-            input: getUrlArgs('n'),
-            results: [],
+            input: getUrlArgs('name'),
             country: c,
-            limit: l,
+            device: d,
             cut: r,
+            limit: l,
+            results: [],
         };
         this.search = this.search.bind(this);
         if (getUrlArgs('n') != null) this.search();
     }
 
     async search() {
-        let { input, country, limit, cut } = this.state;
+        let { input, country, device, limit } = this.state;
         input = input.trim();
         let url = input;
         const itunesReg = /^(http|https):\/\/itunes/;
@@ -42,13 +44,25 @@ class App extends Component {
                 const data = await searchAppById(id, country);
                 this.setState({ results: data.results });
             } else {
-                const data = await Promise.all([
-                    searchIosApp(input, country, limit),
-                    searchMacApp(input, country, limit),
-                ]);
-                this.setState({
-                    results: data[0].results.concat(data[1].results),
-                });
+                if (device == 'all') {
+                    const data = await Promise.all([
+                        searchIosApp(input, country, limit),
+                        searchMacApp(input, country, limit),
+                    ]);
+                    this.setState({
+                        results: data[0].results.concat(data[1].results),
+                    });
+                } else if (device == 'ios') {
+                    const data = await Promise.all([searchIosApp(input, country, limit)]);
+                    this.setState({
+                        results: data[0].results,
+                    });
+                } else if (device == 'mac') {
+                    const data = await Promise.all([searchMacApp(input, country, limit)]);
+                    this.setState({
+                        results: data[0].results,
+                    });
+                }
             }
         } catch (err) {
             console.error(err);
@@ -56,12 +70,13 @@ class App extends Component {
     }
 
     render() {
-        const { input, results, country, limit, cut } = this.state;
+        const { input, country, device, cut, limit, results } = this.state;
         if (input != '') {
-            history.replaceState(null, null, changeUrlArgs('n', input));
-            history.replaceState(null, null, changeUrlArgs('c', country));
-            history.replaceState(null, null, changeUrlArgs('r', cut));
-            history.replaceState(null, null, changeUrlArgs('l', limit));
+            history.replaceState(null, null, changeUrlArgs('name', input));
+            history.replaceState(null, null, changeUrlArgs('country', country));
+            history.replaceState(null, null, changeUrlArgs('device', device));
+            history.replaceState(null, null, changeUrlArgs('cut', cut));
+            history.replaceState(null, null, changeUrlArgs('limit', limit));
         }
         return (
             <div className="app">
@@ -70,31 +85,45 @@ class App extends Component {
                         <div className="logo">HQ ICON</div>
                         <div className="description">从 App Store 获取高清应用图标</div>
                         <div className="options">
-                            <label onClick={() => this.setState({ country: 'CN' })} >
-                                <input name="store" type="checkbox" checked={country === 'CN'} />
+                            <label onClick={() => this.setState({ device: 'ios' })} >
+                                <input name="device" type="checkbox" checked={device === 'ios'} />
+                                iOS
+                            </label>
+                            <label onClick={() => this.setState({ device: 'mac' })} >
+                                <input name="device" type="checkbox" checked={device === 'mac'} />
+                                MacOS
+                            </label>
+                            <label onClick={() => this.setState({ device: 'all' })} >
+                                <input name="device" type="checkbox" checked={device === 'all'} />
+                                同时
+                            </label>
+                        </div>
+                        <div className="options">
+                            <label onClick={() => this.setState({ country: 'cn' })} >
+                                <input name="store" type="checkbox" checked={country === 'cn'} />
                                 中/CN
                             </label>
-                            <label onClick={() => this.setState({ country: 'US' })} >
-                                <input name="store" type="checkbox" checked={country === 'US'} />
+                            <label onClick={() => this.setState({ country: 'us' })} >
+                                <input name="store" type="checkbox" checked={country === 'us'} />
                                 美/US
                             </label>
-                            <label onClick={() => this.setState({ country: 'JP' })} >
-                                <input name="store" type="checkbox" checked={country === 'JP'} />
+                            <label onClick={() => this.setState({ country: 'jp' })} >
+                                <input name="store" type="checkbox" checked={country === 'jp'} />
                                 日/JP
                             </label>
-                            <label onClick={() => this.setState({ country: 'KR' })} >
-                                <input name="store" type="checkbox" checked={country === 'KR'} />
+                            <label onClick={() => this.setState({ country: 'kr' })} >
+                                <input name="store" type="checkbox" checked={country === 'kr'} />
                                 韩/KR
                             </label>
                         </div>
                         <div className="options">
                             <label onClick={() => this.setState({ cut: '1' })} >
                                 <input name="cut" type="checkbox" checked={cut === '1'} />
-                                裁切圆角矩形
+                                裁切圆角
                             </label>
                             <label onClick={() => this.setState({ cut: '0' })} >
                                 <input name="cut" type="checkbox" checked={cut === '0'} />
-                                应用原始图像
+                                原始图像
                             </label>
                         </div>
                         <div className="search">
