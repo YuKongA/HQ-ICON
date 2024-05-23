1cdb5e4523c70f90d307bd6a25c4bdadc989bbb7
余空 <70465933+YuKongA@users.noreply.github.com>
2024-05-23 23:11:55 +0800

    update: Use `axios` instead of `fetch-jsonp`

--- src/searchApp.jsx
+++ src/searchApp.jsx
@@ -1,6 +1,6 @@
-import fetchJsonp from 'fetch-jsonp'
+import axios from 'axios';
 
 export async function searchApp(term, country, entity, limit) {
-  const res = await fetchJsonp(`https://itunes.apple.com/search?term=${encodeURI(term)}&country=${country}&entity=${entity}&limit=${limit}`)
-  return await res.json()
+  const res = await axios.get(`https://itunes.apple.com/search?term=${encodeURI(term)}&country=${country}&entity=${entity}&limit=${limit}`);
+  return res.data;
 }
