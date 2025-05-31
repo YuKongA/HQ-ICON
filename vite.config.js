49cbe13b952b0c9cb25a5dc93b6ad8373d76e5cc
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-05-31 21:36:35 +0800

    update: Use rolldown-vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+  plugins: [
+    react()
+  ],
+  base: './',
 })
